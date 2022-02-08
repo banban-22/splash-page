16f9f059b09f58c8c8526051119f285471e877d6
banban-22 <a1ya1k2a9@gmail.com>
2022-02-08 21:21:47 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const { body } = document;
+
+function changeBackground(number) {
+  // Check if background already showing
+  let previousBackground;
+  if (body.className) {
+    previousBackground = body.className;
+  }
+  // Reset CSS Class for body
+  body.className = '';
+
+  switch (number) {
+    case '1':
+      //   body.classList.add('background-1');
+      return previousBackground === 'background-1'
+        ? false
+        : body.classList.add('background-1');
+      break;
+
+    case '2':
+      //   body.classList.add('background-2');
+      return previousBackground === 'background-2'
+        ? false
+        : body.classList.add('background-2');
+      break;
+
+    case '3':
+      //   body.classList.a\d('background-3');
+      return previousBackground === 'background-3'
+        ? false
+        : body.classList.add('background-3');
+      break;
+
+    default:
+      break;
+  }
+}
